bda65d42d4b67aff13343b313fe16c8a665f2a55
Ruslan-Podvysotsky <podvysutsky00@mail.ru>
2021-12-16 03:29:06 +0200

    first home page

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,27 @@
-// Main js file
-// see more: https://github.com/vedees/webpack-template/blob/master/README.md#import-js-files
 
-console.log('its work')
+function popup () {
+    const choiseBtn = document.querySelector('.js-choice-btn');
+    const choiseCont = document.querySelector('.js-choice-popup');
+    let choiseClose = document.querySelector('body');
+    
+    function showBanner () {
+        choiseCont.classList.add('show');
+    }
+    
+    function removeBanner () {
+        choiseCont.classList.remove('show');
+    }
+
+    function hideBanner (e) {
+        if (e.target.classList.contains('js-close')) {
+            removeBanner();
+        }
+    }
+
+    
+    choiseBtn.addEventListener('click', showBanner);
+    
+    choiseClose.addEventListener('click', hideBanner);
+}
+
+popup();
