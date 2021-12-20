e38cdefa330c71666e5af967955b4680ffaf98e6
Ruslan-Podvysotsky <podvysutsky00@mail.ru>
2021-12-20 23:54:47 +0200

    created home page

--- src/js/index.js
+++ src/js/index.js
@@ -1,27 +1,52 @@
+// animation title
+const animationWrap = document.querySelector('.js-animation-wrap')
 
-function popup () {
-    const choiseBtn = document.querySelector('.js-choice-btn');
-    const choiseCont = document.querySelector('.js-choice-popup');
-    let choiseClose = document.querySelector('body');
-    
-    function showBanner () {
-        choiseCont.classList.add('show');
-    }
-    
-    function removeBanner () {
-        choiseCont.classList.remove('show');
-    }
-
-    function hideBanner (e) {
-        if (e.target.classList.contains('js-close')) {
-            removeBanner();
-        }
-    }
-
-    
-    choiseBtn.addEventListener('click', showBanner);
-    
-    choiseClose.addEventListener('click', hideBanner);
+if (animationWrap) {
+	let title = [...document.querySelectorAll('.js-title-animation')];
+	let i = 0
+	let number = title.length - 1;
+	
+	setInterval(() => {
+		title[i].classList.remove('show');
+		i++;
+		console.log(i);
+		if (i > number) {
+			title[number].classList.remove('show');
+			i = 0
+			title[i].classList.add('show');
+		} else {
+			title[i].classList.add('show');
+		}
+	}, 3900);
 }
 
-popup();
+
+// choise popup
+const choiseCont = document.querySelector('.js-choice-popup');
+
+function popup() {
+  const choiseBtn = document.querySelector('.js-choice-btn');
+  let choiseClose = document.querySelector('body');
+
+  function showBanner() {
+    choiseCont.classList.add('show');
+  }
+
+  function removeBanner() {
+    choiseCont.classList.remove('show');
+  }
+
+  function hideBanner(e) {
+    if (e.target.classList.contains('js-close')) {
+      removeBanner();
+    }
+  }
+
+  choiseBtn.addEventListener('click', showBanner);
+
+  choiseClose.addEventListener('click', hideBanner);
+}
+
+if (choiseCont) {
+	popup();
+}
